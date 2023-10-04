6e37816725301ddbf97f5ef5160fe756a3a85d79
KajsaAviaja <kajsaaviaja@gmail.com>
2023-10-04 15:56:53 +0200

    wip

--- planetZooAnalyser.py
+++ planetZooAnalyser.py
@@ -20,59 +20,92 @@ def analyseAnimal(a):
     td_list = a.find_all("td")
     if len(td_list) > 0:
         name = td_list[0].find("a").text
-        conservation = td_list[1].find("a").find("img", alt=True)["alt"]
+        try:
+            conservation = td_list[1].find("a").find("img", alt=True)["alt"]
 
-        URL = "https://planetzoo.fandom.com/wiki/" + name.replace(" ", "_")
-        page = requests.get(URL)
+            URL = "https://planetzoo.fandom.com/wiki/" + name.replace(" ", "_")
+            page = requests.get(URL)
 
-        content = BeautifulSoup(page.content, "html.parser").find("div", id="content")
+            try:
+                content = BeautifulSoup(page.content, "html.parser").find("div", id="content")
+            except:
+                raise Exception("could not find content")
+            
+            try:
+                continent = content.find("h3", string="Continents").parent.find("div").text
+            except:
+                raise Exception("could not find continent")
+            
+            try:
+                regions = re.split(
+                    ": |, ", content.find("h3", string="Regions").parent.find("div").text
+                )   
+            except:
+                regions = []
 
-        continent = content.find("h3", string="Continents").parent.find("div").text
+            try:
+                type = "Habitat" if (td_list[2].text.strip() == "Full") else "Exhibit"
+            except:
+                raise Exception("could not find type")
+            dict[name] = {
+                "conservation status": conservation,
+                "type": type,
+                "content pack": td_list[3].text.strip(),
+                "continent": continent,
+                "regions": regions,
+            }
 
-        regions = re.split(
-            ": |, ", content.find("h3", string="Regions").parent.find("div").text
-        )
+            if(type == "Habitat"):
+                analyseHabitatAnimal(dict[name],content)
+            else:
+                analyseExhibitAnimal(dict[name],content)
+        
+        except Exception as e:
+            print("error in " + name + ": " + str(e))
+    
 
-        (grade, height, climbproof) = getFenceSpec(content)
+def analyseHabitatAnimal(dict, content):
+    dict["fence"] = getFenceSpec(content)
+    
 
-        dict[name] = {
-            "conservation status": conservation,
-            "type": "Habitat" if (td_list[2].text.strip() == "Full") else "Exhibit",
-            "content pack": td_list[3].text.strip(),
-            "continent": continent,
-            "regions": regions,
-            "fence": {
-                "grade": int(grade),
-                "height": int(height),
-                "climbproof": climbproof,
-            },
-        }
-
+def analyseExhibitAnimal(dict, content):
+    pass
 
 def getFenceSpec(content):
-    fence = (
-        content.find("h3", string="Fence Grade")
-        .parent.parent.find("div")
-        .text.split(" ")
-    )
+
+    try:
+        fence = (
+            content.find("h3", string="Fence Grade")
+            .parent.parent.find("div")
+            .text.replace(">", "").replace("(", "").replace(")", "").replace("m", "").replace("Grade", "").split(" ")
+        )
+    except: 
+        fence = ["-1","-1"]
+        raise Exception("Could not find fence specs")
+        
+ 
+    grade_str = fence[0]
+    height_str = "-1"
+    climbproof = False
+
 
     if len(fence) == 2:
-        return (fence[0], fence[1].replace(">", "").replace("m", ""), False)
+        height_str = fence[1]
+        
     elif len(fence) == 4:
-        return (
-            fence[0],
-            fence[3]
-            .replace(">", "")
-            .replace("(", "")
-            .replace(")", "")
-            .replace("m", ""),
-            True,
-        )
-    else:
-        return (None, None, None)
+            height_str = fence[3]
+            climbproof = True,
+        
+
+    grade = float(grade_str)
+    height = float(height_str)
+
+    return {"grade": grade,"height": height, "climbproof": climbproof,}
+    
+            
+
+for i in range(50,100):
+    analyseAnimal(animal_list[i])
 
 
-analyseAnimal(animal_list[a])
-
-
-print(json.dumps(dict, indent=4))
+#print(json.dumps(dict, indent=4))
